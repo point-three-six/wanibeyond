561ed1b40d641bcc1ccc83c270df340557b675c8
Justin <jhendrix13@gmail.com>
2023-01-31 01:28:13 +1000

    fixed bug where you can't view your own private deck

--- wb-nextjs/src/app/decks/[deckId]/page.tsx
+++ wb-nextjs/src/app/decks/[deckId]/page.tsx
@@ -1,13 +1,26 @@
 import ItemList from './ItemList';
 import prisma from '../../../lib/prisma';
 import InstallButton from '../../components/InstallButton';
+import { getSession } from '../../../lib/session'
 
 export default async function DeckPage({ params: { deckId } }) {
   let id = parseInt(deckId);
+  let sessionData = await getSession();
+
+  let userId = (typeof sessionData.id == 'undefined') ? -1 : sessionData.id;
+
   const deck = await prisma.deck.findFirst({
     where: {
       id: id,
-      isPrivate: false
+      OR: [
+        {
+          isPrivate: false
+        },
+        {
+          isPrivate: true,
+          userId: userId
+        }
+      ]
     },
     include: {
       user: {
@@ -38,21 +51,29 @@ export default async function DeckPage({ params: { deckId } }) {
   });
 
   return (
+
     <>
-      <div className='max-width'>
-        <div className='flex justify-between mb-5'>
-          <div>
-            <div className='font-bold text-xl text-slate-700'>
-              {deck.name}
+      {deck ?
+        <div className='max-width'>
+          <div className='flex justify-between mb-5'>
+            <div>
+              <div className='font-bold text-xl text-slate-700'>
+                {deck.name}
+              </div>
+              Created by {deck.user.username}
             </div>
-            Created by {deck.user.username}
+            <div>
+              <InstallButton deckId={deck.id} />
+            </div>
           </div>
-          <div>
-            <InstallButton deckId={deck.id} />
+          <ItemList deckId={deck.id} items={deck.items} />
+        </div> :
+        <div className='max-width'>
+          <div className='text-center'>
+            This deck does not exist or is private.
           </div>
         </div>
-        <ItemList deckId={deck.id} items={deck.items} />
-      </div>
+      }
     </>
   )
 }
